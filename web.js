e634dcc75bcddd2f1af6facdaa492bdaafa66ef2
Willy Tseng <ophentis@gmail.com>
2013-09-27 14:45:15 +0800

    draft

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
+
 app.use(express.logger());
+app.use('/style',express.static(__dirname + '/style'));
+app.use('/script',express.static(__dirname + '/script'));
+app.use('/image',express.static(__dirname + '/image'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendfile('./html/index.html');
 });
 
 var port = process.env.PORT || 5000;
